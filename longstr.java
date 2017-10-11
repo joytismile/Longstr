632184c062c97e9403fbf825989aa8d6c126dc4d
joytismile <fanghua.geng@gmail.com>
2017-10-11 19:44:43 +0800

    Create longstr.java

---
+++ longstr.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class longstr {
+
+	 public static int lengthOfLongestSubstring(String s) {  
+	        Map<Character,Integer> map=new HashMap<Character,Integer>();  
+	        int maxLength=0;  
+	        int now=0;  
+	        for(int i=0;i<s.length();i++){  
+	            //System.out.println(now);  
+	            //System.out.println(map);  
+	            if(map.containsKey(s.charAt(i))){  
+	                //System.out.println("now:"+now);  
+	                now=Math.max(map.get(s.charAt(i))+1,now);  
+	                if((i-now+1)>maxLength){  
+	                    maxLength=i-now+1;  
+	                      
+	                }  
+	            }else{  
+	                if((i-now+1)>maxLength){  
+	                    maxLength=i-now+1;  
+	                }  
+	            }  
+	            map.put(s.charAt(i), i);  
+	        }  
+	        return maxLength;  
+	    }  
+	    public static void main(String[] args) {  
+	        // TODO Auto-generated method stub  
+	        Scanner sc=new Scanner(System.in);
+	        String str=sc.next();
+	        System.out.println(lengthOfLongestSubstring(str));  
+	    }  
+
+}
